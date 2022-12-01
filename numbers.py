b1a67064196120362a8ca4b3c584f52a01050e80
Andy Albrecht <aalbrecht33733@gmail.com>
2022-12-01 12:01:43 -0500

    Numbers and common operators

--- numbers.py
+++ numbers.py
@@ -1,4 +1,21 @@
-greeting = "Hello World-"
-print(greeting * 99)
+# 'Comments' in Python start as shown, with #.
 
 # Python Numbers:
+# Integers- Whole nums only. + or -. No decimal point.
+
+# Floats- Written with a Decimal point. + or -.
+
+# Python Operators: + - * /. 
+# Order of operations: PMDAS= Parenthesis, Multiply,
+# Divide, Addition, then Subtraction.
+
+# Lesser known operators: 
+# Exponentiation ** - 4 ** 3 = 4 = 4 to the 3rd power.
+
+# Integer Division // - Divides, rounds remainder down.
+
+# Modulo % - 38 % 10 = 8. 10 goes into 38 3 times, 
+# with a remainder of 8
+
+greeting = "Hello World-"
+print(greeting * 99)
