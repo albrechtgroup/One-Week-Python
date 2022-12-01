a7ace2b7342e52c9fcf82fc473876288debd3998
Andy Albrecht <aalbrecht33733@gmail.com>
2022-12-01 13:25:01 -0500

    Variables and assignment operators

---
+++ variables.py
@@ -0,0 +1,37 @@
+# Variable in Python:
+# age = 42
+# age + 1 = 43
+
+# Naming variables:
+# player_one = 0. Underscore is Popular in Python*
+# all capital letter var names indicate a 'const'
+# PI = 3.14259
+
+# score = 500
+# score = score + 500
+
+# Assignment Operators:
+# score += 500
+# score -= 1000
+
+# The 'print' function: The print() func prints out any
+# values we pass to "standard output". It does not
+# return anything.
+
+# MAGIC Exercise:
+# Create a variable called secret_num and set it to any
+# number you'd like.
+secret_num = 7
+# Increase secret_num by 1.
+secret_num += 1
+# Next, take the new number and double it.
+secret_num *= 2
+# Next, take the new number and add four to it.
+secret_num += 4
+# Next, take the new number and divide by 2.
+secret_num /= 2
+# Next, take the new number and subtract your original 
+# number from it.
+secret_num -= 7
+# Print the final result.
+print(secret_num)
