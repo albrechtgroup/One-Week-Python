ac812fe865fa08d18c74d5faf26824b893ab034d
Andy Albrecht <aalbrecht33733@gmail.com>
2022-12-02 15:56:28 -0500

    Strings and escape characters

---
+++ strings.py
@@ -0,0 +1,46 @@
+# Strings wrapped in quotes, same as JS:
+color = "purple"
+
+url = "www.reddit.com/r/formula1/"
+
+# In Python, we can change types of variables:
+age = 42
+age = "forty-three"
+
+# String Operator: 
+# Concatination - Combine two strings
+first_name = "Andrew"
+last_name = "Albrecht"
+
+full_name = first_name + " " + last_name 
+print(full_name)  # Andrew Albrecht
+
+# Multiplication - Multiply a string by a Number
+'ho' * 3 # 'hohoho'
+
+# In Python, strings are 0 Based like JS! Yayyy
+msg = "I <3 Cats"
+ 
+msg[0] # 'I'
+msg[5] # 'C"
+msg[2] # '<'
+msg[-1] # 's'
+
+# None - Is a special value that denotes a lack of 
+# value. Not the same as zero or an empty string.
+# Python's version of 'null'
+user = None
+
+# String Slices:
+msg[2:4] # '<3'
+
+# Print:
+print("Strings SON!!!!")
+
+txt = "Hello WORLD ! " * 33
+print(txt)
+
+# Escape Characters:
+# Newline - \n        # Tab - \t
+# Double Quote - \"   # Single Quote - \'   
+# Backlash - \\
