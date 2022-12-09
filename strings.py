3de9813300d4c122e9c55c7c912d819787d9156d
Andy Albrecht <aalbrecht33733@gmail.com>
2022-12-09 13:37:11 -0500

    Strings and exercises

--- strings.py
+++ strings.py
@@ -13,6 +13,7 @@ first_name = "Andrew"
 last_name = "Albrecht"
 
 full_name = first_name + " " + last_name 
+# Print:
 print(full_name)  # Andrew Albrecht
 
 # Multiplication - Multiply a string by a Number
@@ -34,13 +35,55 @@ user = None
 # String Slices:
 msg[2:4] # '<3'
 
-# Print:
-print("Strings SON!!!!")
-
-txt = "Hello WORLD ! " * 33
-print(txt)
+# txt = "Hello WORLD ! " * 33
+# print(txt)
 
 # Escape Characters:
 # Newline - \n        # Tab - \t
 # Double Quote - \"   # Single Quote - \'   
 # Backlash - \\
+
+# Strings and Functions:
+
+# Arguments = Fancy word for inputs
+
+# the Len() function - will return the lenth of whatever
+# item we pass to it. Excluding numbers.
+word = "Chicken"
+len(word) # 7
+
+# the input() function - will prompt a user to enter 
+# some input, converts it into a string, then returns it.
+age = input("How old are you?")
+
+first_name = input("What is your first name?")
+last_name = input("What is your last name?")
+ 
+full_name = first_name + " " + last_name
+print("Hi there " + full_name)
+
+# Type Casting:
+
+# Age Calculator: 
+age = input("How old are you in years?")
+days = float(age) * 365
+print(days)
+
+# 'F' Strings - are an easy way to generate strings
+# that contain interpolated expressions. Any code 
+# between Curly braces {} will be evaluated and then
+# the result will be turned into a string and inserted
+# into the overall string.
+f"there are {24*60*60} seconds in a day."
+# "there are 86400 seconds in a day."
+print(f"You are {days} old!!")
+
+# Shopping Cart Exercise:
+print("WELCOME TO THE USELESS STORE")
+print("*" * 23)
+item = input("What item are you purchasing? ")
+price = float(input(f"What is the price of {item}? "))
+quantity = float(input(f"How many {item} are you buying? "))
+print(f"Added {quantity} {item}(s) to shopping cart.")
+print(f"Subtotal is: ${price * quantity}")
+
