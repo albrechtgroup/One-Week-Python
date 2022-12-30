a57e419cf94ade18ab7c4ace8fa344d85c8bcf54
Andy Albrecht <aalbrecht33733@gmail.com>
2022-12-29 19:56:45 -0500

    Booleans and comparison operators

---
+++ booleans.py
@@ -0,0 +1,19 @@
+# Booleans and Comparisons: True or False Only
+isLoggedIn = True
+
+# Comparison Operators:
+# == Equal to
+13 == 3 # False
+13 == 13 # True
+
+# != Not equal to
+13 != 3 # True
+13 != 13 # False
+
+# Truthy and Falsey:
+# Falsey = 'False', 0.0, 0, None, range(0), Empty 
+# anything: Empty String, Empty Data Structures [],
+# {}, (), set()
+
+# Truthy = Everthing Else!
+
